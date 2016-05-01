a4bfacb6edf8cf7d463e7f9393b67152f2633e4c
Anne Harris <anneharris4@gmail.com>
2016-05-01 13:09:59 -0700

    Lesson 3 exercises

---
+++ Lesson_3/Basic_Type_Experiments.py
@@ -0,0 +1,30 @@
+num1 = 8
+print num1
+num2 = 2
+print num2
+print num1 + num2
+
+string1 = "Hello"
+print string1
+string2 = "World"
+print string2
+print string1 + string2
+
+numstring1 = "8"
+print numstring1
+numstring2 = "2"
+print numstring2
+print numstring1 + numstring2
+
+print num1 - num2
+print string1 - string2
+print numstring1 - numstring2
+
+print num1 / num2
+print string1 / string2
+print numstring1 / numstring2
+
+print num1 * num2
+print string1 * string2
+print numstring1 * numstring2
+
---
+++ Lesson_3/Types_Exercise_1.py
@@ -0,0 +1,19 @@
+hello = "Hello, world!"
+print hello
+
+name = "Anne and Lydia"
+print hello + name
+
+print hello, name
+print "My name is", name
+print "My name is", name + "!"
+
+num_pets = 0
+print num_pets
+
+print 9596 * 683
+fav_number = 8
+print fav_number - 8
+fav_color = "red"
+
+print fav_color * fav_number
