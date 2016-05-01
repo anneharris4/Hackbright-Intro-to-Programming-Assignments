94ac0ea42b8e781f03c2e43a8ec8418b1bbff690
Anne Harris <anneharris4@gmail.com>
2016-05-01 13:10:47 -0700

    add lesson 4 survey program

---
+++ Lesson_4/Exercise_3_Survey_Program.py
@@ -0,0 +1,8 @@
+print "Welcome to the survey!"
+name = raw_input("What is your name?")
+color = raw_input("What is your favorite color?")
+hobby = raw_input("What is your favorite hobby?")
+movie = raw_input("What is your favorite movie?")
+print name +"'s", "favorite color is", color
+print name +"'s", "favorite hobby is", hobby
+print name+ "'s", "favorite movie is", movie
