3f2b5a235cc94664ec0948f8369b95146b72b8de
Anne Harris <anneharris4@gmail.com>
2016-05-15 18:42:40 -0700

    adding for and while loops initial exercises

---
+++ Lesson_10/for_loops_activity_1.py
@@ -0,0 +1,29 @@
+# question 1
+
+for item in range(1,21):
+	print item
+
+# question 2
+for item in range(1,21):
+	if item == 13:
+		print 'Hello'
+	else:
+		print item 
+
+#question 3
+for item in range(0,101,10):
+	print item
+
+#question 4
+for item in range (1,10,2):
+	print item
+# question 5
+for item in range (10,-1,-1):
+	print item
+
+for item in range (10,-1,-1):
+	if item == 0:
+		print 'Blastoff!'
+	else:
+		print item
+
---
+++ Lesson_10/while_loops_activity_2.py
@@ -0,0 +1,25 @@
+#num = 1
+#while num < 21:
+#	print num
+#	num = num + 1
+
+#num2 = 1 
+
+#while num2 < 21:
+#	if num2 == 13:
+#		print 'Hello'
+#	else:
+#		print num2
+#	num2 = num2 + 1
+
+num3 = 0
+
+while num3 < 101:
+	print num3
+	num3 = num3 + 10 
+
+fruits = ['apples', 'oranges', 'bananas']
+i = 0
+while (i < len(fruits)):
+	print fruits[i]
+	i += 1 
